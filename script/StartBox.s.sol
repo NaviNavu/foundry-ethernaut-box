fb74de3526ccbe4570c38f169b57eb5b4b21f908
NaviNavu <77575346+NaviNavu@users.noreply.github.com>
2023-03-10 19:57:49 +0100

    Fix wrong commit

--- script/StartBox.s.sol
+++ script/StartBox.s.sol
@@ -10,11 +10,17 @@ import { LibLogs } from "internals/libraries/LibLogs.sol";
 import { Box } from "internals/contracts/Box.sol";
 import { EnvironmentData } from "internals/EnvironmentData.sol";
 
-contract StartBox is Script, EnvironmentData, LevelCollection {
+contract StartBox is Script, EnvironmentData {
     function run() public {
-        vm.startBroadcast(envData.playerPK);
+        require(envData.box.code.length == 0, 
+            "The Box is already running. To kill it, restart or quit Anvil."
+        );
 
-        
+        vm.startBroadcast(envData.playerPK);
+        Box box = new Box{salt:"42424242424242424242424242424242"}(envData.ethernaut);
+        vm.stopBroadcast();
+
+        require(address(box) == envData.box, "Wrong Box contract creation salt.");
 
         LibLogs.logBoxCreation(address(box));
     }
